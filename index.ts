502472f94b7d0616d8381540db6e23283d3f2f27
Siddhant-0107 <cvm02874@gmail.com>
2026-09-15 01:27:56 +0530

    Make Vercel entrypoint explicitly Express-compatible

--- index.ts
+++ index.ts
@@ -1,3 +1,8 @@
+import express from "express";
 import app from "./server/index";
+
+// Keep Express as a direct dependency of the Vercel entrypoint so Vercel
+// can reliably detect this file as the server function.
+void express;
 
 export default app;
